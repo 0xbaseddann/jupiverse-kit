7eb23a418a3419445f18ca2451ef3c8eb7fecf87
Dann Wee <dannweezj@gmail.com>
2025-01-03 02:09:31 +0800

    feat: setup web using jupiverse-kit

---
+++ web/src/app/swap/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Swap } from "jupiverse-kit";
+
+const Page = () => {
+  return (
+    <main className="min-h-screen w-full flex items-center justify-center">
+      <Swap
+        rpcUrl={
+          process.env.NEXT_PUBLIC_RPC_URL ||
+          "https://api.mainnet-beta.solana.com"
+        }
+      />
+    </main>
+  );
+};
+
+export default Page;
