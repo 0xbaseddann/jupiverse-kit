72dbffaf2cbe3f6f1717fe472c44fde8643559b1
Dann Wee <dannweezj@gmail.com>
2025-03-02 00:14:43 +0800

    feat: update web ui

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/shared/hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col pb-16">
       <Hero />
       <Playground />
     </main>
